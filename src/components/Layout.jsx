50a7d79386a44ca6df352233484e491d012d6bd0
Artem-Parashchuk <a.p.paraschuk@gmail.com>
2024-10-15 13:20:51 +0300

    update Layout

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,6 +1,10 @@
+import { Navbar } from "./Navbar";
 
-export const Layout = () => {
+export const Layout = ({ children }) => {
   return (
-    <div>Layout</div>
-  )
-}
+    <div className="container mx-auto">
+      <Navbar />
+      {children}
+    </div>
+  );
+};
