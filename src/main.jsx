d7474718a946f29bdff4a98e50c8ae0a77ce48f1
Artem-Parashchuk <a.p.paraschuk@gmail.com>
2024-10-15 13:21:19 +0300

    add store to main

--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,18 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 
+import { Provider } from "react-redux";
+import { store } from "./redux/store.js";
+
 import App from "./App.jsx";
 import "./index.css";
 
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>
     <StrictMode>
-      <App />
+      <Provider store={store}>
+        <App />
+      </Provider>
     </StrictMode>
   </BrowserRouter>
 );
