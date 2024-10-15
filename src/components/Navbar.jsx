eeb1d06f3e95e32ecf25bf002161ba229a5a9526
Artem-Parashchuk <a.p.paraschuk@gmail.com>
2024-10-15 13:19:20 +0300

    update Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,56 @@
+import { Link, NavLink } from "react-router-dom";
 
 export const Navbar = () => {
+  const isAuth = false;
+
+  const activeLink = {
+    color: "white",
+  };
+
   return (
-    <div>Navbar</div>
-  )
-}
+    <div className="flex py-4 justify-between items-center">
+      <span className="flex justify-center items-center w-6 h-6 bg-gray-600 text-xs text-white rounded-sm">
+        E
+      </span>
+      {isAuth && (
+        <ul className="flex gap-8">
+          <li>
+            <NavLink
+              to={"/"}
+              className="text-xs text-grey-400 hover:text-white"
+              style={({ isActive }) => (isActive ? activeLink : null)}
+            >
+              Головна
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to={"/posts"}
+              className="text-xs text-grey-400 hover:text-white"
+              style={({ isActive }) => (isActive ? activeLink : null)}
+            >
+              Мої пости
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to={"/new"}
+              className="text-xs text-grey-400 hover:text-white"
+              style={({ isActive }) => (isActive ? activeLink : null)}
+            >
+              Добавити пост
+            </NavLink>
+          </li>
+        </ul>
+      )}
+
+      <div className="flex justify-center items-center bg-gray-600 text-xs text-white rounded-sm px-4 py-2">
+        {isAuth ? (
+          <button type="button">Вийти</button>
+        ) : (
+          <Link to={"/login"}>Увійти</Link>
+        )}
+      </div>
+    </div>
+  );
+};
