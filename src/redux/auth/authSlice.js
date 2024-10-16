817d01920c12be1d8457088bf44ad14bb84fb07b
Artem-Parashchuk <a.p.paraschuk@gmail.com>
2024-10-16 17:12:41 +0300

    create reducer & extraReducers

--- src/redux/auth/authSlice.js
+++ src/redux/auth/authSlice.js
@@ -1,34 +1,74 @@
 import { createSlice } from "@reduxjs/toolkit";
-import { registerUser } from "./authThunks";
+import { registerUserThunk } from "./registerThunks";
+import { loginThunk } from "./loginThunk";
+import { getMeThunk } from "./getMeThunk";
 
 const initialState = {
-    username: null,
-    token: null,
-    isLoading: false,
-    status: null
+  username: null,
+  token: null,
+  isLoading: false,
+  status: null,
 };
 
 export const authSlice = createSlice({
-    name: 'auth',
-    initialState,
-    reducers: {},
-    extraReducers: (builder) => {
-        builder
-            .addCase(registerUser.pending, (state) => {
-                state.isLoading = true;
-                state.status = null;
-            })
-            .addCase(registerUser.fulfilled, (state, action) => {
-                state.isLoading = false;
-                state.status = action.payload.message;
-                state.username = action.payload.user; 
-                state.token = action.payload.token;   
-            })
-            .addCase(registerUser.rejected, (state, action) => {
-                state.isLoading = false;
-                state.status = action.error?.message || 'Registration failed';
-            });
-    }
+  name: "auth",
+  initialState,
+  reducers: {
+    logout: (state) => {
+      state.isLoading = null;
+      state.status = null;
+      state.token = null; 
+      state.username = null;
+    },
+  },
+  extraReducers: (builder) => {
+    builder
+      // Register User
+      .addCase(registerUserThunk.pending, (state) => {
+        state.isLoading = true;
+        state.status = null;
+      })
+      .addCase(registerUserThunk.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.status = action.payload.message;
+        state.username = action.payload.user;
+        state.token = action.payload.token;
+      })
+      .addCase(registerUserThunk.rejected, (state, action) => {
+        state.status = action.payload.message;
+        state.isLoading = false;
+      })
+      // Login User
+      .addCase(loginThunk.pending, (state) => {
+        state.isLoading = true;
+        state.status = null;
+      })
+      .addCase(loginThunk.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.status = action.payload.message;
+        state.username = action.payload.user;
+        state.token = action.payload.token;
+      })
+      .addCase(loginThunk.rejected, (state, action) => {
+        state.status = action.payload.message;
+        state.isLoading = false;
+      })
+      //Check  authorization
+      .addCase(getMeThunk.pending, (state) => {
+        state.isLoading = true;
+        state.status = null;
+      })
+      .addCase(getMeThunk.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.status = null;
+        state.username = action.payload?.user;
+        state.token = action.payload?.token;
+      })
+      .addCase(getMeThunk.rejected, (state, action) => {
+        state.status = action.payload.message;
+        state.isLoading = false;
+      });
+  },
 });
-
+export const {logout} = authSlice.actions
 export default authSlice.reducer;
